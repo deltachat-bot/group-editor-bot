f490653a0b384ec6f0dc7cec59b1084fe6725633
missytake <missytake@systemli.org>
2024-07-28 19:04:22 +0200

    improve help message

--- group-editor-bot.go
+++ group-editor-bot.go
@@ -134,15 +134,14 @@ func resendPads(rpc *deltachat.Rpc, accId deltachat.AccountId, chatId deltachat.
 }
 
 func sendHelp(rpc *deltachat.Rpc, accId deltachat.AccountId, chatId deltachat.ChatId) {
-	text := "I am a bot that can help you invite friends to your private groups.\n\n"
-	text += "You can also share your own invitation QR with them so why would you need me?\n"
-	text += "If you share your QR, your friends will be able to join only when you are online, but since I am a bot I am always online!\n\n"
-	text += "To get the invitation QR of a group, add me to the group and send in the group:\n\n/invite\n\n"
-	text += "I will share the invitation QR, you can then send it to friends you want to invite.\n\n"
-	text += "If you want to revoque te invitation QR just remove me from the group.\n\n"
+	text := "I am a bot that manages editors in groups.\n\n"
 	text += "To create a new shared editor for the group, you can write:\n\n"
 	text += "/pad Shopping List for Friday's Example Party\n\n"
 	text += "I will send an editor to the group, which anyone can edit; and if new members are added, they will see it, too."
+	text += "I can also help you invite friends to your private groups.\n\n"
+	text += "To get the invite link of a group, add me to the group and send to it:\n\n/invite\n\n"
+	text += "I will share the invitation QR, you can then send it to friends you want to invite.\n\n"
+	text += "If you want to revoke te invitation QR just remove me from the group.\n\n"
 	_, err := rpc.SendMsg(accId, chatId, deltachat.MsgData{Text: text})
 	if err != nil {
 		cli.GetLogger(accId).With("chat", chatId).Error(err)
