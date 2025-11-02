ff57586a524051dde674e81e4965acdd09a2c8a8
missytake <missytake@systemli.org>
2025-11-02 10:24:06 +0100

    tests: create fixtures to simplify tests

---
+++ tests/conftest.py
@@ -0,0 +1,49 @@
+import os
+
+import pytest
+
+from group_editor_bot.hooks import hooks
+
+
+@pytest.fixture
+def bot(acfactory, log):
+    assert os.getenv("CHATMAIL_DOMAIN")
+    log.step("Configuring Bot")
+    bot = acfactory.new_configured_bot()
+    bot.add_hooks(hooks)
+    bot.account.bring_online()
+    return bot
+
+
+@pytest.fixture
+def group(acfactory, bot, log):
+    log.step("Configuring Creator")
+    creator = acfactory.get_online_account()
+
+    log.step("Creator creates group")
+    creator_group = creator.create_group("test_member_added")
+    creator_invite = creator_group.get_qr_code()
+
+    log.step("Bot joins group")
+    bot_group = bot.account.secure_join(creator_invite)
+    bot.account.wait_for_securejoin_joiner_success()
+    bot.account.wait_for_incoming_msg()
+
+    creator_group.creator = creator
+    creator_group.bot_group = bot_group
+    return creator_group
+
+
+@pytest.fixture
+def joiner(acfactory, log):
+    def join_chat(chat):
+        invite = chat.get_qr_code()
+        joiner.secure_join(invite)
+        joiner.wait_for_securejoin_joiner_success()
+        log.step("Joiner receives member_added message")
+        joiner.wait_for_incoming_msg()
+
+    log.step("Configuring Joiner")
+    joiner = acfactory.get_online_account()
+    joiner.join_chat = join_chat
+    return joiner
--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,9 +1,7 @@
-import logging
-
 import pytest
-from deltachat_rpc_client.events import EventType, MemberListChanged, RawEvent
+from deltachat_rpc_client.events import EventType
 
-from group_editor_bot.hooks import catch_events, delete_data, member_added_or_removed
+from group_editor_bot.hooks import delete_data
 
 
 def test_delete_data(acfactory):
@@ -28,40 +26,16 @@ def test_commands(text, file, reply_text, reply_file):
     pytest.skip("Not yet tested")
 
 
-def test_member_added(acfactory, log):
-    log.step("Configuring Bot")
-    bot = acfactory.new_configured_bot()
-    bot.logger = logging
-    bot.add_hook(member_added_or_removed, MemberListChanged)
-    bot.account.bring_online()
-
-    log.step("Configuring Creator and Joiner")
-    creator, joiner = acfactory.get_online_accounts(2)
-
-    log.step("Creator creates group")
-    creator_group = creator.create_group("test_member_added")
-    invite = creator_group.get_qr_code()
-
-    log.step("Bot joins group")
-    bot_group = bot.account.secure_join(invite)
-    bot.account.wait_for_securejoin_joiner_success()
-
-    log.step("Bot receives member_added message")
-    bot.account.wait_for_incoming_msg()
-
+def test_member_added(bot, group, joiner, log):
     log.step("Bot sends test message")
-    msg = bot_group.send_text("Bot sends test message")
+    msg = group.bot_group.send_text("Bot sends test message")
 
     log.step("Creator receives test message")
-    creator_msg = creator.wait_for_incoming_msg()
+    creator_msg = group.creator.wait_for_incoming_msg()
     assert creator_msg.get_snapshot().text == msg.get_snapshot().text
 
     log.step("Joiner joins group")
-    joiner.secure_join(invite)
-    joiner.wait_for_securejoin_joiner_success()
-
-    log.step("Joiner receives member_added message")
-    joiner.wait_for_incoming_msg()
+    joiner.join_chat(group)
 
     log.step("Joiner receives re-sent message")
     bot._process_messages()
@@ -73,39 +47,16 @@ def test_bot_removed(acfactory):
     pytest.skip("Not yet tested")
 
 
-def tests_bot_adds_member(acfactory, log):
-    log.step("Configuring Bot")
-    bot = acfactory.new_configured_bot()
-    bot.logger = logging
-    bot.add_hook(catch_events, RawEvent)
-    bot.account.bring_online()
-
-    log.step("Configuring Creator and Joiner")
-    creator, joiner = acfactory.get_online_accounts(2)
-
-    log.step("Creator creates group")
-    creator_group = creator.create_group("test_member_added")
-    creator_invite = creator_group.get_qr_code()
-
-    log.step("Bot joins group")
-    bot_group = bot.account.secure_join(creator_invite)
-    bot.account.wait_for_securejoin_joiner_success()
-    bot.account.wait_for_incoming_msg()
-
+def tests_bot_adds_member(bot, group, joiner, log):
     log.step("Bot sends test message")
-    msg = bot_group.send_text("Bot sends test message")
+    msg = group.bot_group.send_text("Bot sends test message")
 
     log.step("Creator receives test message")
-    creator_msg = creator.wait_for_incoming_msg()
+    creator_msg = group.creator.wait_for_incoming_msg()
     assert creator_msg.get_snapshot().text == msg.get_snapshot().text
 
     log.step("Joiner joins group")
-    bot_invite = bot_group.get_qr_code()
-    joiner.secure_join(bot_invite)
-    joiner.wait_for_securejoin_joiner_success()
-
-    log.step("Joiner receives member_added message")
-    joiner.wait_for_incoming_msg()
+    joiner.join_chat(group.bot_group)
 
     def joiner_joined(event):
         if event.kind == EventType.SECUREJOIN_INVITER_PROGRESS:
